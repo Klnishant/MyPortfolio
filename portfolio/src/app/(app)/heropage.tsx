a83bc31a0ac9f6fe8c65bd4a2990055e7b34f354
Klnishant <nishantkaushal075@gmail.com>
2024-10-16 21:10:43 +0530

    Change hero page

--- portfolio/src/app/(app)/heropage.tsx
+++ portfolio/src/app/(app)/heropage.tsx
@@ -1,19 +1,22 @@
 'use client'
 import React from 'react'
-import mainImage from "@/media/image/portfolioImage.jpg"
+import mainImage from "@/media/image/portfolioImage.png"
+import { FlipWords } from '@/components/ui/flip-words';
+import { FloatingNav } from '@/components/ui/floating-navbar';
 
 function Heropage() {
-    console.log(mainImage);
+    console.log(mainImage.src);
     
   return (
-    <div className='flex justify-center items-center h-screen'>
-     <div className='flex flex-wrap justify-center items-center gap-10'>
-        <div className='w-1/2'>
-            <h1>Hello I am Nishant Kaushal</h1>
-            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero earum soluta reprehenderit ab neque! Sint itaque porro incidunt odio nobis?</h3>
+    <div>
+     <FloatingNav navItems={[{name:"Home", link: "/"}]} />
+     <div className='flex justify-start items-center  gap-10 text-white'>
+        <div className='w-1/2 p-5 h-1'>
+            <h1 className='text-4xl font-bold'>Hello I am <FlipWords words={["Nisahnt Kaushal", "a FullStack Web Developer", "a Java Developer", "a Data Science Enthusiast"]} duration={3000} className='text-blue-700'/></h1>
+            <h3 className='mt-5 text-xl'>I am a passionate and driven software developer with a strong foundation in Full Stack Web Development and Java Development, coupled with a growing expertise in Data Science.</h3>
         </div>
-        <div>
-            <img src={`${mainImage}`} alt="mainImage" />
+        <div className='w-1/2 h-full flex justify-center items-center z-10'>
+            <img src={`${mainImage.src}`} alt="mainImage" className='h-1/2 w-1/2' />
         </div>
      </div>
     </div>
