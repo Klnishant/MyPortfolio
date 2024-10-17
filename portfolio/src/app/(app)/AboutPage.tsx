82fac96ee8177139022ef33082829eda185b3330
Klnishant <nishantkaushal075@gmail.com>
2024-10-17 20:52:49 +0530

    create about me page

---
+++ portfolio/src/app/(app)/AboutPage.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div className="bg-gray-900 text-white">
+          <div className="w-1/2 p-5">
+            <h2 className="text-blue-700 text-3xl font-bold">About Me</h2>
+            <p className="mt-5">
+            I specialize in Full Stack Web Development and Java Development, with growing expertise in Data Science. Skilled in building web applications using technologies like React, Spring Boot, and Django. Experienced in Java-based backend systems and applying machine learning for data-driven solutions. Always eager to learn and explore new technologies to solve real-world problems.I am continuously learning and excited about the intersection of software engineering and data-driven insights.
+            </p>
+          </div>
+       </div>
+  )
+}
+
+export default AboutPage
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import Heropage from "./(app)/heropage";
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
+import { BackgroundLines } from "@/components/ui/background-lines";
+import AboutPage from "./(app)/AboutPage";
 
 export default function Home() {
   return (
     <div>
       <main className="min-h-screen antialiased">
         <BackgroundBeamsWithCollision  children={<Heropage />} />
-       
+        <AboutPage />
       </main>
     </div>
   );
